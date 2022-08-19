37cd5f2e8c13fbcd38350b220ae3b8af2f83f737
Harrison <harrisonderick.65@gmail.com>
2022-08-19 18:00:21 +0100

    chore: app fixed for upload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [gameState, setGameState] = useState(false);
   const [time, setTime] = useState(30);
 //   const [database, setDatabase] = useState(fetchQuestions);
-  const database = fetchQuestions;
+  const database = fetchQuestions();
   var prizes = [
     100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000,
     250000, 500000, 1000000
